56a0d258309e3476b35856043eba7ba39f21a478
Jeppe <jeppe.moelgaard3@gmail.com>
2023-09-07 15:31:54 +0200

    Refactore index on get

--- Frontend/Pages/Index.cshtml.cs
+++ Frontend/Pages/Index.cshtml.cs
@@ -31,31 +31,7 @@ namespace mtg_aspnet_v2.Pages
         public async Task OnGetAsync()
         {
             this.Sets = await FetchSets();
-            // foreach (string setname in Sets)
-            // {
-            //     Console.WriteLine(setname);
-            // }
-            //Refactor to own function
-            if (HttpContext.Session.GetInt32("UserId") != null && HttpContext.Session.GetString("UserName") != null)
-            {
-                this.UserId = HttpContext.Session.GetInt32("UserId"); //(int)HttpContext.Session.GetInt32("UserId");
-                this.UserName = HttpContext.Session.GetString("UserName");
-                try
-                {
-                    string cardsData = await _clientApi.GetCardsByUserId((int)this.UserId);
-                    if (!string.IsNullOrEmpty(cardsData))
-                    {
-                        //Console.WriteLine(cardsData);
-                        var cardList = JsonConvert.DeserializeObject<List<Card>>(cardsData);
-                        List<string> cardTitles = cardList.Select(c => c.Title).ToList();
-                        this.Cards = cardTitles;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            this.Cards = await DisplayOwnedCards();
         }
 
         public async Task<List<string>> FetchSets()
@@ -77,61 +53,111 @@ namespace mtg_aspnet_v2.Pages
             }
         }
 
+        public async Task<List<string>> DisplayOwnedCards()
+        {
+            if (HttpContext.Session.GetInt32("UserId") != null && HttpContext.Session.GetString("UserName") != null)
+            {
+                this.UserId = HttpContext.Session.GetInt32("UserId"); //(int)HttpContext.Session.GetInt32("UserId");
+                this.UserName = HttpContext.Session.GetString("UserName");
+                try
+                {
+                    string cardsData = await _clientApi.GetCardsByUserId((int)this.UserId);
+                    if (!string.IsNullOrEmpty(cardsData))
+                    {
+                        //Console.WriteLine(cardsData);
+                        var cardList = JsonConvert.DeserializeObject<List<Card>>(cardsData);
+                        List<string> cardTitles = cardList.Select(c => c.Title).ToList();
+                        return cardTitles;
+                    }
+                    else
+                    {
+                        return new List<string>();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            }
+            return null;
+        }
 
+        public class UserResponse
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public List<string> Cards { get; set; }
+        }
+
+        public class Card
+        {
+            public string Title { get; set; }
+        }
+
+
+        public class Set
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+
+            [JsonIgnore]
+            public List<string> Booster { get; set; }
+            public DateTime ReleaseDate { get; set; }
+            public string Block { get; set; }
+            public bool OnlineOnly { get; set; }
+        }
     }
-
-    public class UserResponse
-    {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public List<string> Cards { get; set; }
-    }
-
-    public class Card
-    {
-        public string Title { get; set; }
-    }
-
-
-
-    // public class SetsResponse
-    // {
-    //     public List<string> Sets { get; set; }
-    // }
-
-    public class Set
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-
-        [JsonIgnore]
-        public List<string> Booster { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Block { get; set; }
-        public bool OnlineOnly { get; set; }
-    }
-
-    // public class SetsResponse
-    // {
-    //     public List<Set> Sets { get; set; }
-    // }
-
-    // public class Set
-    // {
-    //     public string Code { get; set; }
-    //     public string Name { get; set; }
-    //     public string Type { get; set; }
-    //     public List<string> Booster { get; set; }
-    //     public DateTime ReleaseDate { get; set; }
-    //     public string Block { get; set; }
-    //     public bool OnlineOnly { get; set; }
-    // }
-
-
 }
 
+
+// public class SetsResponse
+// {
+//     public List<string> Sets { get; set; }
+// }
+
+
+// public class SetsResponse
+// {
+//     public List<Set> Sets { get; set; }
+// }
+
+// public class Set
+// {
+//     public string Code { get; set; }
+//     public string Name { get; set; }
+//     public string Type { get; set; }
+//     public List<string> Booster { get; set; }
+//     public DateTime ReleaseDate { get; set; }
+//     public string Block { get; set; }
+//     public bool OnlineOnly { get; set; }
+// }
+
+
+
+// //Refactor to own function
+// if (HttpContext.Session.GetInt32("UserId") != null && HttpContext.Session.GetString("UserName") != null)
+// {
+//     this.UserId = HttpContext.Session.GetInt32("UserId"); //(int)HttpContext.Session.GetInt32("UserId");
+//     this.UserName = HttpContext.Session.GetString("UserName");
+//     try
+//     {
+//         string cardsData = await _clientApi.GetCardsByUserId((int)this.UserId);
+//         if (!string.IsNullOrEmpty(cardsData))
+//         {
+//             //Console.WriteLine(cardsData);
+//             var cardList = JsonConvert.DeserializeObject<List<Card>>(cardsData);
+//             List<string> cardTitles = cardList.Select(c => c.Title).ToList();
+//             this.Cards = cardTitles;
+//         }
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine(ex.Message);
+//     }
+// }
 
 // public void OnGet()
 // {
